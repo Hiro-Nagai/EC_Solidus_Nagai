e7279a15ae090bf8622e670d96975d06429a486f
Hiro-Nagai <gm.komabun2003@gmail.com>
2022-12-18 18:06:15 +0900

    Rspec route conf

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -3,5 +3,48 @@ require 'rails_helper'
 RSpec.describe "Products", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
+
+  end
+end
+
+describe "routes", type: :routing do
+  it "ルートresources :products, only: [:show]確認" do
+    expect(get: "potepan/products").to route_to(controller: "products", action: "show")
+  end
+end
+
+
+
+
+
+
+
+
+
+=begin
+require 'rails_helper'
+
+RSpec.describe "Potepan::Products", type: :request do
+  describe "GET /show" do
+    let(:test_taxon) { create(:taxon) }
+    let(:test_product) { create(:product, taxons: [test_taxon]) }
+    let(:products) { create_list(:product, 5, taxons: [test_product.taxons.first]) }
+
+    before do
+      get potepan_product_path(test_product.id)
+    end
+
+    it "httpステータスが200を返すこと" do
+      expect(response).to have_http_status "200"
+    end
+
+    it "意図したページへrenderできているか" do
+      expect(response).to render_template(:show)
+    end
+
+    it "同じtaxon情報を持つ他のproductを４つまで返すこと" do
+      stub_const("RELATED_PRODUCTS_DISPLAY_NUM_MAX", 4)
+      expect(test_product.related_products.limit(RELATED_PRODUCTS_DISPLAY_NUM_MAX)).to eq [products[0], products[1], products[2], products[3]]
+    end
   end
 end
