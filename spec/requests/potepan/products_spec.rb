a5348a6356c28233831faa35d50452717cd32c2b
Hiro-Nagai <gm.komabun2003@gmail.com>
2022-12-19 22:43:34 +0900

    PR amended for indication, rest is  RSpec

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,16 +1,63 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Products", type: :request do
-  describe "GET /show" do
-    let(:product) { create(:spree_product, :skip_validate) }
+RSpec.describe 'Products_request', type: :request do
+  describe 'GET /potepan/products/:id' do
+    
+    
+    let!(:product) { create(:product,) }
 
     before do
-      get potepan_product_url(product.id)
+      get potepan_products_path(product.id)
     end
 
+    it 'returns 200 response' do
+      expect(response).to have_http_status(200)
+    end
 
+    it 'shows a product information' do
+      expect(response.body).to include product.name
+      expect(response.body).to include product.display_price.to_s
+      expect(response.body).to include product.description
+    end
   end
 end
 
 
 
+
+
+
+
+=begin
+rescue => exception
+  
+end
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Products_request', type: :request do
+  describe 'GET /potepan/products/:id' do
+
+    
+    
+    !let(:product) { create(:product)  } 
+
+    before do
+      get potepan_products_path(product.id)
+    end
+
+    it 'returns 200 response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'shows a product information' do
+      expect(response.body).to include product.name
+      expect(response.body).to include product.display_price.to_s
+      expect(response.body).to include product.description
+    end
+  end
+end
+=end
