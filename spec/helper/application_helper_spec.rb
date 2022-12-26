6aed0e3571014aaba6489ba5d3107f754edf87eb
Hiro-Nagai <gm.komabun2003@gmail.com>
2022-12-26 18:13:49 +0900

    指摘事項改善bary822

--- spec/helper/application_helper_spec.rb
+++ spec/helper/application_helper_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "full_titleメソッドのテスト" do
-    context "特定のタイトルを指定した場合" do
-      it "「product.name - BIGBAG Store」を出力すること" do
+    context "特定のタイトルを指定した場合（引数が指定された場合）" do
+      it "「引数 - BIGBAG Store」を出力すること" do
         expect(helper.full_title("product_name")).to eq "product_name - BIGBAG Store"
       end
     end
 
-    context "特定のタイトルを指定しない場合(nilの場合)" do
+    context "特定のタイトルを指定しない場合(引数がnilの場合)" do
       it "「BIGBAG Store」を出力すること" do
         expect(helper.full_title(nil)).to eq "BIGBAG Store"
       end
     end
 
-    context "特定のタイトルを指定しない場合(空文字の場合)" do
+    context "特定のタイトルを指定しない場合(引数が空文字の場合)" do
       it "「BIGBAG Store」を出力すること" do
         expect(helper.full_title("")).to eq "BIGBAG Store"
       end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  RSpec.configure do |config|
-    config.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   end
 end
