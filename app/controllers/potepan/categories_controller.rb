b551bc0b64918e2e2c2566499ba7ccfa6bafa27f
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-05 23:47:43 +0900

    viewほぼ完 残課題sidebar カテゴリ表示　テスト　

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.all
     @taxon = Spree::Taxon.find(params[:id])
+    @products = @taxon.all_products.includes(master: [:default_price, :images])
   end
 end
