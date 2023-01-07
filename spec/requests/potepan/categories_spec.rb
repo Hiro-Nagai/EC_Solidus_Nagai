36c2ce63d3270802bf5e57ff2b5beee9a1869bfb
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-07 22:44:59 +0900

    指摘事項改善bary822

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -16,8 +16,26 @@ RSpec.describe "Potepan::Categories", type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it "カテゴリーの名前が表示されていること" do
+    it "親カテゴリー名(Taxonomy名)が表示されていること" do
+      expect(response.body).to include taxonomy.name
+    end
+
+    it "ページ内の複数箇所に表示されているTaxon名が全てもれなく表示されていること" do
       expect(response.body).to include taxon.name
+    end
+
+    it "Taxon名に引き続いてそれに紐づく商品数が表示されていること" do
+      expect(response.body).to include taxon.products.size.to_s
+    end
+
+    it "対象のTaxonに紐付いていない商品が表示されていないこと" do
+      expect(response.body).not_to include()
+    end
+
+    it "意図した商品画像が表示されていること" do
+      within ('.item') do 
+        expect(page).to have_selector("img[src$='blank.jpg']")
+      end
     end
 
     it "商品の名前価格が表示されていること" do
