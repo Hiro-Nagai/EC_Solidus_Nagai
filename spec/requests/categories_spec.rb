c395328713cbacb8f60828116b560d38ead91677
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-10 23:44:52 +0900

    2指摘事項改善shimizzuu

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,44 +1,14 @@
 RSpec.describe "Potepan::Categories", type: :request do
   describe "categories/show" do
-    let(:image) { create(:image) }
-    let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon) }
     let(:product) { create(:product, taxons: [taxon]) }
 
     before do
-      product.images << image
       get potepan_category_path(taxon.id)
     end
 
     it "カテゴリーページが表示されていること" do
       expect(response).to have_http_status(:success)
-    end
-
-    it "親カテゴリー名(Taxonomy名)が表示されていること" do
-      expect(response.body).to include taxonomy.name
-    end
-
-    it "ページ内の複数箇所に表示されているTaxon名が全てもれなく表示されていること" do
-      expect(response.body).to include taxon.name
-    end
-
-    it "Taxon名に引き続いてそれに紐づく商品数が表示されていること" do
-      expect(response.body).to include taxon.products.size.to_s
-    end
-
-    it "対象のTaxonに紐付いていない商品が表示されていないこと" do
-      expect(response.body).not_to include()
-    end
-
-    it "意図した商品画像が表示されていること" do
-      within ('.item') do 
-        expect(page).to have_selector("img[src$='blank.jpg']")
-      end
-    end
-
-    it "商品の名前価格が表示されていること" do
-      expect(response.body).to include product.name
-      expect(response.body).to include product.display_price.to_s
     end
   end
 end
---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Potepan::Products", type: :system do
+  describe "GET #show" do
+    let(:product) { create(:product, taxons: [taxon]) }
+    let(:taxonomy) { create(:taxonomy) }
+    let(:taxon) { create(:taxon, taxonomy: taxonomy) }   
+    
+    before do
+      get potepan_product_path(product.id)
+    end
+
+    it "ページが表示されていること" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
