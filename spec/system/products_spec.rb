14d3bb39138fbdae3687a2d2d13772c676404de0
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-10 21:49:13 +0900

    systemspec visitへ変更 test通った

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -7,12 +7,18 @@ RSpec.describe "Potepan::Products", type: :system do
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon, taxonomy: taxonomy) }
     
+    
     before do
       product.images << image
       get potepan_product_path(product.id)
       # 画像URL取得が上手くいかない問題への対応
       # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
       ActiveStorage::Current.host = request.base_url
+    end
+
+    it "一覧ページへ戻るをクリックすると商品カテゴリページへアクセスされていること" do
+      visit potepan_category_path(taxon.id)
+      expect(current_path).to eq potepan_category_path(taxon.id)
     end
 
     it "ページが表示されていること" do
