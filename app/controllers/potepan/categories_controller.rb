b97b0519248febf3a37b3ce79cb0013195f09d88
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-14 14:33:35 +0900

    1.N+1へcategory_controller修正 2.categories_specへテス追加

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    @taxonomies = Spree::Taxonomy.all
+    @taxonomies = Spree::Taxonomy.includes(taxons: :products)
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.all_products.includes(master: [:default_price, :images])
   end
