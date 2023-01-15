927d131b54461b2a52c0442395facdf9dcea9427
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-15 19:41:01 +0900

    エラー有 一旦push　view,decorator,controller実装中　

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @related_product = @product.related_products.limit(4)
-
+    @taxons = Spree::Taxon.find(@product.taxons.ids)
+    @related_products = @product.related_products
   end
 end
--- app/models/potepan/product_decorater.rb
+++
@@ -1,7 +0,0 @@
-module Potepan::ProductDecorator
-    def ralated_products
-        Spree::Product.in_taxons(taxons).includes(master: [:images, :default_price]).where.not(id: id).order("RAND()").distinct
-    end
-
-        Spree::Product.prepend self
-end
