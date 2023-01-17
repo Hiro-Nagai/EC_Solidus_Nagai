296f2b12018a006ca8822ad1fefc7d31dc95ffb9
Hiro-Nagai <gm.komabun2003@gmail.com>
2023-01-17 09:54:47 +0900

    rubocopさまご指摘修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,7 @@ class Potepan::ProductsController < ApplicationController
   RELATED_PRODUCTS_SHOW_NUM = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.distinct.limit(RELATED_PRODUCTS_SHOW_NUM).includes(master: [:default_price, images: { attachment_attachment: :blob }])
+    @related_products = @product.related_products.distinct.limit(RELATED_PRODUCTS_SHOW_NUM)
+    .includes(master: [:default_price, images: { attachment_attachment: :blob }])
   end
 end
